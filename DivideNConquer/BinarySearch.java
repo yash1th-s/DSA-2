99561dd0d0e17f8205f50751590da329b465c0f2
Yashwanth S <yashwanths0210@gmail.com>
2022-11-08 21:10:22 +0530

    .

--- DivideNConquer/BinarySearch.java
+++ DivideNConquer/BinarySearch.java
@@ -17,9 +17,27 @@ public class BinarySearch {
         return -1;
     }
 
+    //recursive binary search
+    public static int search(int arr[], int target, int start, int end){
+        if(start>end){
+            return -1;
+        }
+
+        int mid = start + (end-start)/2;
+        if(arr[mid] == target){
+            return mid;
+        }
+
+        if(arr[mid]<=target){
+            return search(arr, target, mid+1, end);
+        }else{
+            return search(arr, target, start, mid-1);
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = {2, 4, 6, 8, 10, 15, 17};
-        System.out.println(search(arr, 10));
+        System.out.println(search(arr,8, 0, arr.length-1));
     }
     
 }
